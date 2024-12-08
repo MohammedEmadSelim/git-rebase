e130b0f23fa3a6e98670a0b698c5088f8cfdff50
mohamed.emad <mohammedemad557@yahoo.com>
2024-12-08 23:59:05 +0200

    commit work around

--- bin/git_rebase.dart
+++ bin/git_rebase.dart
@@ -2,4 +2,5 @@ import 'package:git_rebase/git_rebase.dart' as git_rebase;
 
 void main(List<String> arguments) {
   print('Hello world: ${git_rebase.calculate()}!');
+  print('the is branch change');
 }
